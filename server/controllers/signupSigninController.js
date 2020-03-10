93715502249e54dfbf3300630f5f3e0a5d2b5971
Ally4 <el.ally741@gmail.com>
2020-03-10 16:11:12 +0200

    chore(storage):database setting-of-database-and-set-of-validations [finishes #171717825]

--- server/controllers/signupSigninController.js
+++ server/controllers/signupSigninController.js
@@ -1,13 +1,14 @@
 import bcrypt from 'bcrypt';
 import dotenv from 'dotenv';
 import jwt from 'jsonwebtoken';
-import users from '../models/users';
-import signupValidations from '../validators/signupSigninValidators';
+import pool from '../database/configuration';
+import { signupValidations } from '../validators/signupValidators';
 
 dotenv.config();
 
 class signupSigninController {
-  static signup(req, res) {
+  static async signup(req, res) {
+    const signup = await pool.query('SELECT * FROM users WHERE email = $1', [req.body.email]);
     const { error } = signupValidations.validate(req.body);
     if (error) {
       return res.status(400).json({
@@ -15,26 +16,22 @@ class signupSigninController {
         message: error.details[0].message.replace(/"/g, ''),
       });
     }
-    const emailSignup = users.find(i => i.email === req.body.email);
-    if (emailSignup) {
+    if (signup.rows && signup.rows.length > 0) {
       return res.status(409).json({
         status: 409,
-        message: 'The email you are trying to sign with is already in use',
+        message: 'The email you are trying to use is already in use',
       });
     }
     const password = bcrypt.hashSync(req.body.password, 10);
-    const Id = users.length + 1;
     const {
-      firstname, lastname, email,
+      firstname, lastname, othername, email, phonenumber, passporturl,
     } = req.body;
-    users.push({
-      Id, firstname, lastname, email, password,
-    });
+    await pool.query('INSERT INTO users (firstname, lastname, othername, email, password, phonenumber, passporturl) VALUES ($1, $2, $3, $4, $5, $6, $7)', [firstname, lastname, othername, email, password, phonenumber, passporturl]);
     const payload = { email, firstname };
     const token = jwt.sign(payload, process.env.KEYWORD);
     return res.status(201).json({
       status: 201,
-      message: 'User have been created successfully',
+      message: 'User have been regidtered in the system successfully',
       data: {
         token,
       },
---
+++ server/database/createTableUsers.js
@@ -0,0 +1,66 @@
+import dotenv from 'dotenv';
+import pool from './configuration';
+
+dotenv.config();
+
+const createtableusers = async () => {
+  const users = `CREATE TABLE IF NOT EXISTS
+      users (
+          id serial primary key,
+          firstname VARCHAR,
+          lastname VARCHAR,
+          othername VARCHAR,
+          email VARCHAR,
+          password VARCHAR,
+          phonenumber INT,
+          passporturl VARCHAR,
+          isadmin VARCHAR DEFAULT 'false'
+      )`;
+  const party = `CREATE TABLE IF NOT EXISTS
+        party (
+            id serial primary key,
+            name VARCHAR,
+            hqaddress VARCHAR,
+            logourl VARCHAR
+        )`;
+  const office = `CREATE TABLE IF NOT EXISTS
+        office (
+            id serial primary key,
+            type VARCHAR,
+            name VARCHAR
+        )`;
+  const candidate = `CREATE TABLE IF NOT EXISTS
+        candidate (
+            id serial primary key,
+            office INT,
+            party INT,
+            candidate INT
+        )`;
+  const vote = `CREATE TABLE IF NOT EXISTS
+        vote (
+            id serial primary key,
+            createdon TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            createdby INT,
+            office INT,
+            candidate INT
+        )`;
+  const petition = `CREATE TABLE IF NOT EXISTS
+        petition (
+            id serial primary key,
+            createdon TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            createdby INT,
+            office INT,
+            body VARCHAR
+        )`;
+  await pool.query(users);
+  await pool.query(party);
+  await pool.query(office);
+  await pool.query(candidate);
+  await pool.query(vote);
+  await pool.query(petition);
+  console.log('The table is created');
+};
+
+createtableusers();
+
+export default createtableusers;
